28da47a964e182f1031a363f36fb6746633419c5
Kamanovich <121173578+Kamanovich@users.noreply.github.com>
2023-09-18 19:43:44 +0300

    Add files via upload

---
+++ knut.js
@@ -0,0 +1,6 @@
+let check = prompt ('Введите число');
+let sum = 0;
+for (let i = 1; i <= check; i++) {
+    sum += i; 
+}
+alert (sum)
---
+++ olb.js
@@ -0,0 +1,14 @@
+const a1 = '*'
+const a2 = '**'
+const a3 = '***'
+const a4 = '****'
+const a5 = '*****'
+const a6 = '******'
+const a7 = '*******'
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+console.log(a6);
+console.log(a7);
